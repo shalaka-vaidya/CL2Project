f611527987a848bddc14abc846d355eae0c8bc5d
shalaka-vaidya <shalaka18101994@gmail.com>
2019-04-12 16:16:57 +0530

    graph

--- tree_parsing.py
+++ tree_parsing.py
@@ -7,6 +7,8 @@ import nltk
 import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import re
+from indic_transliteration import xsanscript
+from indic_transliteration.xsanscript import transliterate
 
 def relationDic(tree):
 	pcrelation=[]
@@ -35,15 +37,16 @@ def graphMaking(tree):
 	plt.figure()
 	labelDic={}
 	nodeLabels={}
-	nodeLabels[0]="dummy"
+	nodeLabels['0']="dummy"
 	for word in tree:
-		print(word)
+		#print(word)
 		nodetup=(word[0],word[6])
 		graph.add_edge(nodetup[0],nodetup[1])
 		labelDic[nodetup]=word[7]
-		nodeLabels[nodetup[0]]=nodetup[0]
+		actualWord=word[1]
+		nodeLabels[nodetup[0]]=transliterate(actualWord, xsanscript.DEVANAGARI, xsanscript.HK)
 	pos=nx.spring_layout(graph)
-	nx.draw(graph,pos,edge_color='black',width=1,linewidths=1,node_size=500,node_color='pink',alpha=0.9,labels={node:node for node in graph.nodes()})
+	nx.draw(graph,pos,edge_color='black',width=1,linewidths=1,node_size=500,node_color='pink',alpha=0.9,labels=nodeLabels)
 	nx.draw_networkx_edge_labels(graph,pos,edge_labels=labelDic)
 	plt.axis('off')
 	plt.show()
@@ -67,6 +70,7 @@ def parsing(fname):
 			print ("SENTENCE", sent[i])
 			words=sent[i].split()
 			tree= parser.parse(words)
+			print(tree)
 			graph=graphMaking(tree)
 			mainWord=printMainVerb(tree)
 			if mainWord is not None:
